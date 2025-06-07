9ea3d326dc1b0e880b3a040f3c1e5518fa7ffb5d
Nedim Faiku <80713994+NedimFaiku@users.noreply.github.com>
2025-06-07 11:41:25 +0200

    changed the logic

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ if not os.path.exists(CSV_FILE):
         writer = csv.writer(file)
         writer.writerow(['temperature_F', 'humidity', 'pressure_inHg', 'wind_dir', 'wind_speed_mph', 'solar_radiation', 'uv_index'])
 
-@app.route('/ecowitt', methods=['GET'])  # Changed POST to GET
+@app.route('/ecowitt', methods=['GET'])  # changed from POST to GET
 def receive_data():
-    data = request.args.to_dict()  # Changed form to args
+    data = request.args.to_dict()  # changed from request.form to request.args
 
-    # Extract relevant fields (adjust as needed)
+    # Extract relevant fields
     row = [
         data.get('tempf'),
         data.get('humidity'),
@@ -34,5 +34,4 @@ def receive_data():
     return "OK"
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
